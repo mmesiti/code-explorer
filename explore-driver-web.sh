e6d04387c59575b0163a2800ae65ddcfaeb52266
Michele Mesiti <michele.mesiti@kit.edu>
2025-03-16 17:24:02 +0100

    new driver: repos from web + browse src on github
    unfortunately local repos cannot be opened from browser
    for security reasons, apparently

---
+++ explore-driver-web.sh
@@ -0,0 +1,14 @@
+#!/bin/bash --login
+SRC="$(cd "$(dirname "${BASH_SOURCE[0]}" )" > /dev/null 2>&1 && pwd)"
+
+REPO="$1"
+DEFAULT_BRANCH="$2"
+LOCALDIR=$(mktemp -d)
+
+git clone --depth=1 $REPO $LOCALDIR
+
+TMPDB=$(mktemp)
+cloc --sql=1 "$LOCALDIR" | sqlite3 "$TMPDB"
+
+cd "$SRC"
+poetry run ./plot-db.py "$TMPDB" $(basename "$LOCALDIR").cloc.db.html $REPO $LOCALDIR $DEFAULT_BRANCH
--- plot-db.py
+++ plot-db.py
@@ -4,21 +4,61 @@ import sqlite3
 import pandas as pd
 import plotly.express as px
 
+def get_path_and_link(origpath,localrepopath,repolink,sep,default_branch="main"): 
+    localpath = origpath
+    path_from_reporoot = localpath.replace(localrepopath,'')
+    path_split = path_from_reporoot.split('/')
+    dirname = path_split[:-1]
+    basename = path_split[-1]
+    path =  repolink.replace('.git','') +f"/blob/{default_branch}/" + path_from_reporoot 
+
+    res = sep.join(dirname) + sep + '<a href="{}" style="color: #000000; text-decoration: underline;">{}</a>'.format(path,basename)
+    return res 
+
+
+def main(cloc_db_filename,
+        output_fig_filename,
+        repolink=None,
+        localrepopath=None,
+        default_branch=None):
+    connection = sqlite3.connect(cloc_db_filename)
+    df = pd.read_sql_query("SELECT * from t;",connection)
+    connection.close()
+
+    separator_for_trick = "####"
+    paths = (df["File"]
+            .apply(lambda p: 
+                get_path_and_link(p, localrepopath, repolink, separator_for_trick,default_branch)
+                if repolink else p.replace('/',separator_for_trick))
+            .str.split(separator_for_trick,expand=True))
+
+    df_expanded = df.merge(paths, 
+                           left_index=True,
+                           right_index=True)
+   
+
+    fig = px.treemap(df_expanded, 
+                     path=list(range(0,len(paths.columns))),
+                     color="Language",
+                     values="nCode")
+    
+    fig.show()
+    print(f"Writing file {output_fig_filename}")
+    fig.write_html(output_fig_filename)
+
 cloc_db_filename = argv[1]
 output_fig_filename = argv[2]
 
-connection = sqlite3.connect(cloc_db_filename)
-df = pd.read_sql_query("SELECT * from t;",connection)
-connection.close()
-paths = df["File"].str.split('/',expand=True)
-df_expanded = df.merge(paths, 
-                       left_index=True,
-                       right_index=True)
+try:
+    repolink=argv[3]
+    localrepopath=argv[4]
+    default_branch=argv[5]
+except:
+    repolink=None
+    localrepopath=None
+    default_branch=None
 
-fig = px.treemap(df_expanded, 
-                 path=list(range(0,len(paths.columns))),
-                 color="Language",
-                 values="nCode")
+    pass
 
-fig.show()
-fig.write_html(output_fig_filename)
+main(cloc_db_filename,output_fig_filename,repolink,localrepopath,default_branch)
+
